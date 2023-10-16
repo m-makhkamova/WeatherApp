84a8fa41852b01c15e84c1782c9034b43d4af9e0
User_3 <rmuhammadqodir58@gmail.com>
2023-10-16 09:51:22 +0500

    commit

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,15 +2,19 @@ package com.example.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.volley.RequestQueue
 import com.android.volley.Response
+import com.android.volley.Response.Listener
 import com.android.volley.VolleyError
 import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.example.weatherapp.databinding.ActivityMainBinding
 import org.json.JSONArray
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     var binding = ActivityMainBinding.inflate(layoutInflater)
@@ -20,26 +24,23 @@ class MainActivity : AppCompatActivity() {
 
         val url = "http://api.weatherapi.com/v1/forecast.json?key=34775b5a5d5f4d92ada34706231010&q=Tashkent&days=6&aqi=no&alerts=no"
         val requestQueue: RequestQueue = Volley.newRequestQueue(this)
-        val request = JsonArrayRequest(url, object :Response.Listener<JSONArray> {
-            override fun onResponse(response: JSONArray?) {
-                for (i in 0 until response!!.length()) {
-                    val obj = response.getJSONObject(i)
-                    var temp = obj.getString("temp")
-                    var icon = obj.getString("icon")
+        var request = JsonObjectRequest(url, object :Response.Listener<JSONObject>{
+            override fun onResponse(response: JSONObject?) {
+                val resobj = response!!.getJSONObject("location")
+                val region = resobj.getString("region")
+                val current = response.getJSONObject("current")
+                val condition = current.getJSONObject("condition")
+                val weatherIcon = "http:"+condition.getString("icon")
+                binding.temp.text = current.getString("temp")
 
-                    binding.temp.text = temp
 
-                }
             }
-        },
+                                                                                  },
             object :Response.ErrorListener{
                 override fun onErrorResponse(error: VolleyError?) {
-
+                    Log.d("TAG", "onErrorResponse: $error")
                 }
-            })
+                                                                                  })
 
-        requestQueue.add(request)
-
-        binding.rv.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
     }
 }
